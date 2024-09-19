2a65e6c28b7c0863d34fba19e05601e5337ec682
Jacob <110055706+frog187man@users.noreply.github.com>
2024-09-19 10:46:59 -0700

    added redirects

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Verify.css'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
